6de7ec47a2cc8eeb01b9dedfd2d430ed1f8fc9f8
Jianli <zhang.jianli@live.com>
2022-08-25 14:40:43 -0400

    change 0825

--- Demo_CIFAR10_Classi.py
+++ Demo_CIFAR10_Classi.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
+import ssl
 from tqdm import tqdm
-dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+ssl._create_default_https_context = ssl._create_unverified_context
 
 data = torchvision.datasets.CIFAR10(
     root='./CIFAR10', train=True, download=True,
@@ -15,7 +16,7 @@ data = torchvision.datasets.CIFAR10(
 )
 train_data, val_data = random_split(data, (45000, len(data) - 45000))
 train_dl = DataLoader(train_data, batch_size=128, shuffle=True)
-val_dl = DataLoader(val_data,batch_size=len(val_data))
+val_dl = DataLoader(val_data,batch_size=100)
 
 #%%
 import torch.nn as nn
@@ -60,31 +61,39 @@ if os.path.exists('CIFAR10_checkpoint.pt'):
     best_model = checkpoint['best_model']
     best_acc = checkpoint['best_acc']
     epoch0 = checkpoint['epoch']
+    print('Existing model loaded.')
 else:
     best_acc = float('-inf')
     epoch0 = 0
     best_model = None
+    print('New model created.')
 #%%
+# dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+dev = 'cpu'
+model.to(dev)
+loss_fn.to(dev)
+print(f'learning using {dev}')
 for epoch in range(epoch0, 20 + epoch0):
     t0 = time.time()
     model.train()
     for img, label in tqdm(train_dl):
-        # img, label = img.to(dev), label.to(dev)
+        img, label = img.to(dev), label.to(dev)
         optimizer_fn.zero_grad()
         pred = model(img)
         loss = loss_fn(pred, label)
         loss.backward()
         optimizer_fn.step()
     model.eval()
-    for img, label in val_dl:
-        # img, label = img.to(dev), label.to(dev)
+    tot_acc = 0
+    for img, label in tqdm(val_dl):
+        img, label = img.to(dev), label.to(dev)
         pred = model(img)
         loss = loss_fn(pred, label)
         pred = torch.argmax(pred, dim=1)
-        acc = torch.sum(torch.eq(pred, label)) / len(label)
-    print(f"Epoch {epoch}: loss = {loss}, accuracy = {acc.item()}, time = {time.time() - t0:.2f}")
-    if acc > best_acc:
-        best_acc = acc
+        tot_acc += torch.sum(torch.eq(pred, label)) / len(label)
+    print(f"Epoch {epoch}: loss = {loss:.4f}, accuracy = {tot_acc/50:.4f}, time = {time.time() - t0:.2f}")
+    if tot_acc/50 > best_acc:
+        best_acc = tot_acc/50
         best_model = copy.deepcopy(model)
 #%%
 checkpoint = {
@@ -95,7 +104,6 @@ checkpoint = {
     'best_acc': best_acc
 }
 torch.save(checkpoint,'CIFAR10_checkpoint.pt')
-
 
 #%%
 imgs = torch.tensor(data.data[:625]).permute((0,3,1,2))
--- Demo_CIFAR10_Classi_Plus.py
+++ Demo_CIFAR10_Classi_Plus.py
@@ -16,62 +16,103 @@ data = torchvision.datasets.CIFAR10(
     transform=transforms.ToTensor()
 )
 train_data, val_data = random_split(data, (45000, len(data) - 45000))
-train_dl = DataLoader(train_data, batch_size=400, shuffle=True)
-val_dl = DataLoader(val_data,batch_size=len(val_data))
+train_dl = DataLoader(train_data, batch_size=128, shuffle=True)
+val_dl = DataLoader(val_data,batch_size=100)
 #%%
 import torch.nn as nn
-def conv_block(in_, out_):
-    return nn.Sequential(
-        nn.Conv2d(in_, out_, kernel_size=3, padding=1),
-        nn.BatchNorm2d(out_),
-    )
-
 class Demo_Model(nn.Module):
+    def conv_block(self, in_, out_, hasPool=False):
+        layers:nn.Sequential = nn.Sequential(
+            nn.Conv2d(in_, out_, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_),
+        )
+        if hasPool:
+            layers.append(nn.MaxPool2d(2))
+        return layers
+
     def __init__(self, in_, out_) -> None:
         super().__init__()
-        self.in_ = in_
-        self.out_ = out_
+        self.conv1 = self.conv_block(in_, 64)
+        self.conv2 = self.conv_block(64, 128, True)
+        self.res1 = nn.Sequential(
+            self.conv_block(128, 128), 
+            self.conv_block(128, 128)
+        )
 
-        self.conv1 = conv_block(self.in_, 64)
-        self.conv2 = conv_block(64, 128)
-        self.max_pool1 = nn.MaxPool2d(2)
+        self.conv3 = self.conv_block(128, 256, True)
+        self.conv4 = self.conv_block(256, 512, True)
+        self.res2 = nn.Sequential(
+            self.conv_block(512, 512), 
+            self.conv_block(512, 512)
+        )
 
-    def forward(self, x):
-        out = self.conv1(x) # 64*32*32
-        out = self.conv2(out) # 128*32*32
-        res1 = self.max_pool1(out) # 128*16*16
-        out = conv_block(128, 128)(res1)
-        out = conv_block(128, 128)(out)
-        out = out + res1 #! res1 is the residual
-        out = nn.MaxPool2d(4)(out) # 128*4*4
-        out = nn.Flatten()(out) # 2048
-        out = nn.Dropout(0.2)(out)  #! zero input tensor elements
-        out = nn.Linear(2048, self.out_)(out) # 10
+        self.classifier = nn.Sequential(
+            nn.MaxPool2d(4), nn.Flatten(), 
+            nn.Dropout(0.2),nn.Linear(512, out_)
+        )
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
         return out
 
-
-model = Demo_Model(3, 10)
 #%%
+dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = Demo_Model(3, 10)
+model.to(dev)
 optimizer_fn = torch.optim.Adam(model.parameters(), lr=0.001)
 loss_fn = torch.nn.CrossEntropyLoss()
+loss_fn.to(dev)
+
+import copy
+import os
+if os.path.exists('CIFAR10_checkpoint.pt'):
+    checkpoint = torch.load('CIFAR10_checkpoint.pt')
+    model.load_state_dict(checkpoint['model'])
+    optimizer_fn.load_state_dict(checkpoint['opt'])
+    best_model = checkpoint['best_model']
+    best_acc = checkpoint['best_acc']
+    epoch0 = checkpoint['epoch']
+    print('Existing model loaded.')
+else:
+    best_acc = float('-inf')
+    epoch0 = 0
+    best_model = None
+    print('New model created.')
+
 for epoch in range(20):
     t0 = time.time()
     model.train()
     for img, label in tqdm(train_dl):
-        # img, label = img.to(dev), label.to(dev)
+        img, label = img.to(dev), label.to(dev)
         optimizer_fn.zero_grad()
         pred = model(img)
         loss = loss_fn(pred, label)
         loss.backward()
         optimizer_fn.step()
     model.eval()
+    acc = 0
     for img, label in val_dl:
-        # img, label = img.to(dev), label.to(dev)
+        img, label = img.to(dev), label.to(dev)
         pred = model(img)
         loss = loss_fn(pred, label)
         pred = torch.argmax(pred, dim=1)
-        acc = torch.sum(torch.eq(pred, label)) / len(label)
-    print(f"Epoch {epoch}: loss = {loss}, accuracy = {acc.item()}, time = {time.time() - t0:.2f}")
-    if acc > best_acc:
-        best_acc = acc
+        acc += torch.sum(torch.eq(pred, label)) / len(label)
+    print(f"Epoch {epoch}: loss = {loss:.4f}, accuracy = {acc/50:.4f}, time = {time.time() - t0:.2f}")
+    if acc/50 > best_acc:
+        best_acc = acc/50
         best_model = copy.deepcopy(model)
+#%%
+checkpoint = {
+    'epoch': epoch + 1,
+    'model': model.state_dict(),
+    'opt': optimizer_fn.state_dict(),
+    'best_model': best_model,
+    'best_acc': best_acc
+}
+torch.save(checkpoint,'CIFAR10_checkpoint.pt')
